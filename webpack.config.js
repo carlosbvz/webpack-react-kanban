98720ca8134b333322e15e1abab1e1a134e7fc61
Carlos Benavides V <carlos.benavides@digitaslbi.com>
2016-10-14 11:06:32 -0600

    webpack file first changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,39 @@
-const path = require('path');
-
-const PATHS = {
+const merge 	= require('webpack-merge');
+const webpack 	= require('webpack');
+const TARGET 	= process.env.npm_lifecyle_event;
+const PATHS 	= {
 	app: 	path.join(__dirname,'app'),
 	build: 	path.join(__dirname,'build')
-}
-
-module.exports = {
+};
+const common = {
 	entry: {
-		app: PATHS.app
+		app: PATHS.app,
 	},
 	output: {
 		path: PATHS.build,
 		filename: 'bundle.js'
 	}
+};
+
+// DEFAULT config
+if(TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devServer: {
+			contentBase: PATHS.build,
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			stats: 'errors-only',
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
+
+if(TARGET === 'build') {
+	module.export = merge(common,{});
 }
